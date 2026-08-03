cb09fd286389ed32320f27d4a737c59d395d2288
alejandronbachi <alejandronbachi@gmail.com>
2026-08-03 09:24:25 -0300

    changed infobar layout to center lbl_status

--- widgets/infor_bar.py
+++ widgets/infor_bar.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QFrame, QHBoxLayout, QLabel
+from PyQt6.QtWidgets import QFrame, QGridLayout, QLabel
 
 from config import ui_constants
 
@@ -8,33 +8,36 @@ class InfoBar(QFrame):
     def __init__(self, parent):
         super().__init__(parent)
         self.setObjectName(ui_constants.WIDGET_INFO_BAR)
-
-        self.layout = QHBoxLayout(self)
+        self.layout = QGridLayout(self)
         self.layout.setContentsMargins(10, 5, 10, 5)
 
         self._build_ui()
 
     def _build_ui(self):
-        # Left directory name label
+        # Left directory name label (Column 0)
         self.lbl_directory = QLabel("")
-        self.lbl_directory.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.lbl_directory.setObjectName("lbl_directory")
-        self.layout.addWidget(self.lbl_directory)
+        self.lbl_directory.setAlignment(
+            Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter
+        )
+        self.lbl_directory.setObjectName("lblDirectory")
+        self.lbl_directory.setVisible(False)
+        self.layout.addWidget(self.lbl_directory, 0, 0)
 
-        self.layout.addStretch(1)
-
-        # Primary Centered File Status Label
+        # Primary Centered File Status Label (Column 1)
         self.lbl_status = QLabel(ui_constants.TEXT_READY_STATUS)
         self.lbl_status.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.lbl_status.setObjectName("lblStatus")
-        self.layout.addWidget(self.lbl_status)
+        self.layout.addWidget(self.lbl_status, 0, 1)
 
-        self.layout.addStretch(1)
-
-        # Secondary Right Edge Metrics Tracker
+        # Secondary Right Edge Metrics Tracker (Column 2)
         self.lbl_metrics = QLabel("")
         self.lbl_metrics.setAlignment(
             Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter
         )
         self.lbl_metrics.setObjectName("lblMetrics")
-        self.layout.addWidget(self.lbl_metrics)
+        self.layout.addWidget(self.lbl_metrics, 0, 2)
+
+        # 2. Force side columns to stretch equally, locking the center in place
+        self.layout.setColumnStretch(0, 1)
+        self.layout.setColumnStretch(1, 0)  # Center only takes required space
+        self.layout.setColumnStretch(2, 1)
